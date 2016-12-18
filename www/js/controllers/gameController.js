906d35066a0b37057d094c2c9e8feec2e34f733d
leadpython <jeffrey.campecino@gmail.com>
2016-12-18 01:00:08 -0800

    added game animations

--- www/js/controllers/gameController.js
+++ www/js/controllers/gameController.js
@@ -1,7 +1,57 @@
 angular.module('darknet-hacker')
 
-.controller('gameController', function($scope, $location) {
+.controller('gameController', function($scope, $location, $interval, gameService) {
+  $scope.$on('$ionicView.enter', () => {
+    $scope.gameOptions = {
+      name: '',
+      imageUrl: '',
+      mode: '',
+      reward: 0,
+      tries: 0,
+      timeLimit: 90,
+      drainRate: 0.01
+    };
+    setGameOptions();
+    beginGame();
+  });
   $scope.goTo = (location) => {
     $location.path('/' + location);
+  }
+  let beginGame = () => {
+    // check if triangulation mode is on
+    if ($scope.gameOptions.mode === 'triangulation') {
+      initiateTimeLimit();
+    }
+    // check if darknet mode is on
+    if ($scope.gameOptions.mode === 'darknet') {
+      initiateDrainDefense();
+    }
+  }
+  let setGameOptions = () => {
+    $scope.gameOptions.mode = gameService.selectedLevel.mode;
+    $scope.gameOptions.reward = gameService.selectedLevel.reward;
+    $scope.gameOptions.name = gameService.selectedLevel.name;
+    $scope.gameOptions.imageUrl = gameService.selectedLevel.imageUrl;
+  }
+  let initiateTimeLimit = () => {
+    $scope.animations.timeLimitAnimation = $interval(() => {
+      $scope.gameOptions.timeLimit--;
+      if ($scope.gameOptions.timeLimit <= 0) {
+        $interval.cancel($scope.animations.timeLimitAnimation);
+      }
+    }, 1000);
+  }
+  let initiateDrainDefense = () => {
+    $scope.animations.drainDefenseAnimation = $interval(() => {
+      $scope.gameOptions.reward = $scope.gameOptions.reward * (1.0 - $scope.gameOptions.drainRate);
+      if ($scope.gameOptions.reward <= 1) {
+        $scope.gameOptions.reward
+        $interval.cancel($scope.animations.drainDefenseAnimation);
+      }
+    }, 1000);
+  }
+  let stopDefenseAnimations = () => {
+    $interval.cancel($scope.animations.timeLimitAnimation);
+    $interval.cancel($scope.animations.drainDefenseAnimation);
   }
 })
--- www/js/services/gameService.js
+++ www/js/services/gameService.js
@@ -2,8 +2,8 @@ angular.module('darknet-hacker')
 
 .factory('gameService', function() {
   let service = {};
-
   service.selectedLevel = {};
+  service.secretCode = 0;
   service.setLevel = (level) => {
     service.selectedLevel = level;
     switch (service.selectedLevel.mode) {
@@ -17,6 +17,46 @@ angular.module('darknet-hacker')
         service.selectedLevel.reward = service.selectedLevel.reward * 5;
         break;
     }
+  }
+  service.generateCode = () => {
+    
+  }
+  service.checkCode = (guess) => {
+    let secretCodeArr = secretCode.split('');
+    let guessArr = guess.split('');
+    let feedback = {
+      green: 0,
+      yellow: 0,
+      doesItWin: false
+    };
+
+    // check greens
+    for (let i = 0; i < secretCodeArr.length; i++) {
+      if (secretCodeArr[i] === guessArr[i]) {
+        feedback.green++;
+        delete secretCodeArr[i];
+        delete guessArr[i];
+      }
+    }
+
+    if (green = secretCodeArr.length) {
+      feedback.doesItWin = true;
+      return feedback;
+    }
+
+    // check yellows
+    for (let i = 0; i < secretCodeArr.length; i++) {
+      for (let j = 0; j < guessArr.length; j++) {
+        if (secretCodeArr[i] && guessArr[j]) {
+          if (secretCodeArr[i] === guessArr[j]) {
+            feedback.yellow++;
+            delete secretCodeArr[i];
+            delete guessArr[j];
+          }
+        }
+      }
+    }
+    return feedback;
   }
 
   return service;
